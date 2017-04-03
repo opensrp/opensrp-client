ce8b210774f6102952b4eb9c1286a75421deecf5
mib.iqbal <mib.iqbaal@gmail.com>
2017-04-03 17:16:28 +0800

    MI | enable form-submission sync service #433
    Signed-off-by: mib.iqbal <mib.iqbaal@gmail.com>

--- opensrp-app/src/main/java/org/ei/opensrp/service/FormSubmissionSyncService.java
+++ opensrp-app/src/main/java/org/ei/opensrp/service/FormSubmissionSyncService.java
@@ -59,7 +59,7 @@ public class FormSubmissionSyncService {
             mCloudantSyncHandler.startPushReplication();
 
             mCountDownLatch.await();
-            //pushToServer();
+            pushToServer();
             Intent intent = new Intent(DrishtiApplication.getInstance().getApplicationContext(),ImageUploadSyncService.class);
             DrishtiApplication.getInstance().getApplicationContext().startService(intent);
             return FetchStatus.fetched;
