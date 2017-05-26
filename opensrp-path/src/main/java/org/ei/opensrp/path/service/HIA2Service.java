e17e0b95d6dc3bd4dd9404abdc0071023c1a1320
James Maina <jmaina@ona.io>
2017-05-26 16:55:00 +0300

    Fix: weight indicators queries #507
    Signed-off-by: James Maina <jmaina@ona.io>

--- opensrp-path/src/main/java/org/ei/opensrp/path/service/HIA2Service.java
+++ opensrp-path/src/main/java/org/ei/opensrp/path/service/HIA2Service.java
@@ -40,7 +40,31 @@ public class HIA2Service {
     private static String CHN2_015_DHIS_ID = "xIGHv5CY2fF";
     private static String CHN2_020 = "CHN2-020";
     private static String CHN2_020_DHIS_ID = "H5cadfqRh7I";
+    private static String CHN2_025 = "CHN2-025";
+    private static String CHN2_025_DHIS_ID = "xWDkbLq9kji";
+    private static String CHN2_030 = "CHN2-030";
+    private static String CHN2_030_DHIS_ID = "e10sC5c4pRz";
+    private static String CHN2_035 = "CHN2-035";
+    private static String CHN2_035_DHIS_ID = "lcpx7xdVC3z";
+    private static String CHN2_040 = "CHN2-040";
+    private static String CHN2_040_DHIS_ID = "hi9sRtkzimM";
+    private static String CHN2_041 = "CHN2-041";
+    private static String CHN2_041_DHIS_ID = "unknown";
+    private static String CHN2_045 = "CHN2-045";
+    private static String CHN2_045_DHIS_ID = "LpkrzZezPhP";
+    private static String CHN2_050 = "CHN2-050";
+    private static String CHN2_050_DHIS_ID = "AzLJv6qTtPO";
+    private static String CHN2_051 = "CHN2-051";
+    private static String CHN2_051_DHIS_ID = "unknown";
+    private static String CHN2_055 = "CHN2-055";
+    private static String CHN2_055_DHIS_ID = "gdrQ69fCF8B";
+    private static String CHN2_060 = "CHN2-060";
+    private static String CHN2_060_DHIS_ID = "ke26q8KPQPM";
+    private static String CHN2_061 = "CHN2-061";
+    private static String CCHN2_061_DHIS_ID = "unknown";
 
+
+//FIXME to uniquely identify out of areas change group by child.base_entity_id to group by zeir_id
     public static void generateIndicators(final SQLiteDatabase database, int month) {
 
     }
@@ -100,7 +124,8 @@ public class HIA2Service {
     }
 
     /**
-     * Number of total children < 5 who attended a clinic this month 	"[CHN1-011] + [CHN1-021]
+     * Number of total children < 5 who attended a clinic this month
+     * "[CHN1-011] + [CHN1-021]
      * [Non-editable in the form]"
      *
      * @param db
@@ -142,7 +167,8 @@ public class HIA2Service {
 
     /**
      * Number of total children weighed aged < 5 years who attended  clinic this month	"[CHN2-005] + [CHN2-010]
-     [Non-editable in the form]"
+     * [Non-editable in the form]"
+     *
      * @param db
      */
     private void getCHN2_015(SQLiteDatabase db) {
@@ -152,14 +178,144 @@ public class HIA2Service {
     /**
      * Number of children age 0-23 months who where weighed for = 2 consecutive months who did not gain >100g of weight in those months
      * COUNT number of children 0-23 months [Date_Birth] with [weight current visit - weight previous visits < 100g] who had = 2 consecutive weight encounters at this clinic
+     *
      * @param db
      */
     private void getCHN2_020(SQLiteDatabase db) {
 
-        String query="select child.base_entity_id as beid,strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) as currentweightdate,(w.kg*1000) as currentweight," +
-                "(select (pw.kg*1000) from weights pw where pw.base_entity_id=w.base_entity_id and strftime('%Y-%m',datetime(pw.date/1000, 'unixepoch')) <strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) and strftime('%Y-%m',date(datetime(w.date/1000, 'unixepoch'),'-1 months'))='2017-03' order by pw.date desc limit 1) as prevweight,\n" +
-                "(select (pw.kg*1000) from weights pw where pw.base_entity_id=w.base_entity_id and  strftime('%Y-%m',datetime(pw.date/1000, 'unixepoch')) <strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) and strftime('%Y-%m',date(datetime(w.date/1000, 'unixepoch'),'-2 months'))='2017-02' order by pw.date desc limit 1 ) as last2monthsweight\n" +
-                "from weights w left join ec_child child on w.base_entity_id=child.base_entity_id where currentweightdate='2017-04' group by beid";
+        String query = "select child.base_entity_id as beid,strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) as currentweightdate,(w.kg*1000) as currentweight," +
+                "(select (pw.kg*1000) from weights pw where pw.base_entity_id=w.base_entity_id  and strftime('%Y-%m',datetime(pw.date/1000, 'unixepoch'))=strftime('%Y-%m',date('now'),'-1 months')  limit 1) as prevweight," +
+                "(select (pw.kg*1000) from weights pw where pw.base_entity_id=w.base_entity_id  and strftime('%Y-%m',datetime(pw.date/1000, 'unixepoch'))=strftime('%Y-%m',date('now'),'-2 months')  limit 1 ) as last2monthsweight," +
+                ageQuery() +
+                "from weights w left join ec_child child on w.base_entity_id=child.base_entity_id where strftime('%Y-%m',date('now'))=currentweightdate and age <23 and (currentweight-prevweight>0 and prevweight-last2monthsweight>0) group by beid";
+
+    }
+
+    /**
+     * Number of children 24-59 months who where weighed for = 2 consecutive months who did not gain >100g of weight in those months
+     * COUNT number of children 24-59 months [Date_Birth]  with [weight current visit - weight previous visits < 100g] who had = 2 consecutive weight encounters at this clinic
+     *
+     * @param db
+     */
+    private void getCHN2_025(SQLiteDatabase db) {
+
+        String query = "select child.base_entity_id as beid,strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) as currentweightdate,(w.kg*1000) as currentweight," +
+                "(select (pw.kg*1000) from weights pw where pw.base_entity_id=w.base_entity_id  and strftime('%Y-%m',datetime(pw.date/1000, 'unixepoch'))=strftime('%Y-%m',date('now'),'-1 months')  limit 1) as prevweight," +
+                "(select (pw.kg*1000) from weights pw where pw.base_entity_id=w.base_entity_id  and strftime('%Y-%m',datetime(pw.date/1000, 'unixepoch'))=strftime('%Y-%m',date('now'),'-2 months')  limit 1 ) as last2monthsweight," +
+                ageQuery() +
+                "from weights w left join ec_child child on w.base_entity_id=child.base_entity_id where strftime('%Y-%m',date('now'))=currentweightdate and age between 24 and 59 and (currentweight-prevweight>0 and prevweight-last2monthsweight>0) group by beid";
+
+    }
+
+    /**
+     * Number of total children age < five years who where weighed for = 2 consecutive months who did not gain >100g of weight in those months
+     * "[CHN2-020] + [CHN2-025]
+     * [Non-editable in the form]"
+     *
+     * @param db
+     */
+    private void getCHN2_030(SQLiteDatabase db) {
+
+    }
+
+    /**
+     * Number of total children age 0-23 months whose weight is between -2Z and -3Z scores
+     *
+     * @param db
+     */
+    private void getCHN2_035(SQLiteDatabase db) {
+        String query="select"+ageQuery() +
+                "from weights w left join ec_child child on w.base_entity_id=child.base_entity_id" +
+                "where strftime('%Y-%m',date('now'))=strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) and age<=23 and w.z_score between -2 and -3 group by child.base_entity_id;";
+
+    }
+
+    /**
+     * Number of total children age 24-59 months whose weight is between -2Z and -3Z scores
+     *
+     * @param db
+     */
+    private void getCHN2_040(SQLiteDatabase db) {
+        String query="select"+ageQuery() +
+                "from weights w left join ec_child child on w.base_entity_id=child.base_entity_id" +
+                "where strftime('%Y-%m',date('now'))=strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) and age between 24 and 59 and w.z_score between -2 and -3 group by child.base_entity_id;";
+
+    }
+
+    /**
+     * Number of total children age < 5 years whose weight is between -2Z and -3Z scores
+     * "[CHN2-035] + [CHN2-040]
+     * [Non-editable in the form]"
+     *
+     * @param db
+     */
+    private void getCHN2_041(SQLiteDatabase db) {
+
+    }
+
+    /**
+     * Number of total children age 0-23 months whose weight is below -3Z scores
+     *
+     * @param db
+     */
+    private void getCHN2_045(SQLiteDatabase db) {
+        String query="select"+ageQuery() +
+                "from weights w left join ec_child child on w.base_entity_id=child.base_entity_id" +
+                "where strftime('%Y-%m',date('now'))=strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) and age<=23 and w.z_score< -3 group by child.base_entity_id;";
+    }
+
+    /**
+     * Number of total children age 24-59 months whose weight is below -3Z scores
+     *
+     * @param db
+     */
+    private void getCHN2_050(SQLiteDatabase db) {
+        String query="select"+ageQuery() +
+                "from weights w left join ec_child child on w.base_entity_id=child.base_entity_id" +
+                "where strftime('%Y-%m',date('now'))=strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) and age between 24 and 59 and w.z_score < -3 group by child.base_entity_id;";
+    }
+
+    /**
+     * Number of total children age < 5 years whose weight below -3Z scores
+     * [CHN2-045] + [CHN2-050]
+     * [Non-editable in the form]
+     *
+     * @param db
+     */
+    private void getCHN2_051(SQLiteDatabase db) {
+
+    }
+
+    /**
+     * Number of total children age 0-23 months whose weight is above 2Z scores
+     *
+     * @param db
+     */
+    private void getCHN2_055(SQLiteDatabase db) {
+        String query="select"+ageQuery() +
+                "from weights w left join ec_child child on w.base_entity_id=child.base_entity_id" +
+                "where strftime('%Y-%m',date('now'))=strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) and age<=23 and w.z_score>2 group by child.base_entity_id;";
+
+
+    }
+
+    /**
+     * Number of total children age 24-59 months whose weight is above 2Z scores
+     * @param db
+     */
+    private void getCHN2_060(SQLiteDatabase db) {
+        String query="select"+ageQuery() +
+                "from weights w left join ec_child child on w.base_entity_id=child.base_entity_id" +
+                "where strftime('%Y-%m',date('now'))=strftime('%Y-%m',datetime(w.date/1000, 'unixepoch')) and age between 24 and 59 and w.z_score >2 group by child.base_entity_id;";
+
+
+    }
+
+    /**
+     * Number of total children age < 5 years whose weight is above 2Z scores
+     * @param db
+     */
+    private void getCHN2_061(SQLiteDatabase db) {
 
     }
 
