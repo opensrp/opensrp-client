b628bdae3cdc5b6b334cca1010c6615df382d415
Ephraim Muhia <emuhia@ona.io>
2017-07-10 17:44:48 +0300

    Update: added report empty state illustration #519
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- opensrp-path/src/main/java/org/ei/opensrp/path/fragment/DraftMonthlyFragment.java
+++ opensrp-path/src/main/java/org/ei/opensrp/path/fragment/DraftMonthlyFragment.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.ei.opensrp.path.R;
@@ -41,7 +42,7 @@ public class DraftMonthlyFragment extends Fragment {
     private AlertDialog alertDialog;
     private DraftsAdapter draftsAdapter;
     private ListView listView;
-    private TextView noDraftsText;
+    private View noDraftsView;
 
     public static DraftMonthlyFragment newInstance() {
         DraftMonthlyFragment fragment = new DraftMonthlyFragment();
@@ -57,7 +58,7 @@ public class DraftMonthlyFragment extends Fragment {
         final View fragmentview = inflater.inflate(R.layout.sent_monthly_fragment, container, false);
 
         listView = (ListView) fragmentview.findViewById(R.id.list);
-        noDraftsText = (TextView) fragmentview.findViewById(R.id.empty_view);
+        noDraftsView =  fragmentview.findViewById(R.id.empty_view);
         startNewReportEnabled = (Button) fragmentview.findViewById(R.id.start_new_report_enabled);
         startNewReportDisabled = (Button) fragmentview.findViewById(R.id.start_new_report_disabled);
 
@@ -151,7 +152,7 @@ public class DraftMonthlyFragment extends Fragment {
 
     private void updateDraftsReportListView(final List<MonthlyTally> monthlyTallies) {
         if (monthlyTallies != null && !monthlyTallies.isEmpty()) {
-            noDraftsText.setVisibility(View.GONE);
+            noDraftsView.setVisibility(View.GONE);
             listView.setVisibility(View.VISIBLE);
             if (draftsAdapter == null) {
                 draftsAdapter = new DraftsAdapter(monthlyTallies);
@@ -161,7 +162,7 @@ public class DraftMonthlyFragment extends Fragment {
                 draftsAdapter.notifyDataSetChanged();
             }
         } else {
-            noDraftsText.setVisibility(View.VISIBLE);
+            noDraftsView.setVisibility(View.VISIBLE);
             listView.setVisibility(View.GONE);
         }
     }
