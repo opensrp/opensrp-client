f2ede029869768fa39ce40ab7430cdf5cd299e2e
habib09 <mPowerhabib>
2017-07-18 19:18:46 +0600

    HA|WOMAN register's view and large log added

---
+++ opensrp-app/src/main/java/org/ei/opensrp/logger/Logger.java
@@ -0,0 +1,19 @@
+package org.ei.opensrp.logger;
+
+import android.util.Log;
+
+/**
+ * Created by habib on 7/18/17.
+ */
+public class Logger {
+
+    public static void largeLog(String tag, String content) {
+        if (content.length() > 4000) {
+            Log.d(tag, content.substring(0, 4000));
+            largeLog(tag, content.substring(4000));
+        } else {
+            Log.d(tag, content);
+        }
+    }
+
+}
